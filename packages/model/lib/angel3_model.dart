86538f7af1d5afd1c0bcfed4156e679c86b6005b
thomashii@dukefirehawk.com <thomashii@dukefirehawk.com>
2023-01-02 09:01:11 +0800

    Updated models

--- packages/model/lib/angel3_model.dart
+++ packages/model/lib/angel3_model.dart
@@ -1,6 +1,4 @@
-//library angel3_model;
-
-/// Represents arbitrary data, with an associated ID and timestamps.
+/// Represents a generic data model with an ID and timestamps.
 class Model {
   /// A unique identifier corresponding to this item.
   String? id;
@@ -14,5 +12,24 @@ class Model {
   Model({this.id, this.createdAt, this.updatedAt});
 
   /// Returns the [id], parsed as an [int].
-  int get idAsInt => id != null ? int.tryParse(id!) ?? -1 : -1;
+  int get idAsInt => id != null ? int.tryParse(id ?? "-1") ?? -1 : -1;
+
+  /// Returns the [id] or "" if null.
+  String get idAsString => id ?? "";
+}
+
+/// Represents a generic data model with audit log feature.
+class AuditableModel extends Model {
+  /// The authorized user who created the record.
+  String? createdBy;
+
+  /// The user who updated the record last time.
+  String? updatedBy;
+
+  AuditableModel(
+      {super.id,
+      super.createdAt,
+      this.createdBy,
+      super.updatedAt,
+      this.updatedBy});
 }
