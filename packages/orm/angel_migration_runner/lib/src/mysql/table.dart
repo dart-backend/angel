3803fd0aceccde6b064a861f744d32312f901682
Martin Zander <mzander@notebooksbilliger.de>
2024-07-08 11:54:56 +0200

    add standard index handling for mariadb and mysql

--- packages/orm/angel_migration_runner/lib/src/mysql/table.dart
+++ packages/orm/angel_migration_runner/lib/src/mysql/table.dart
@@ -56,6 +56,14 @@ abstract class MySqlGenerator {
     return buf.toString();
   }
 
+  static String? compileIndex(String name, MigrationColumn column) {
+    if (column.indexType == IndexType.standardIndex) {
+      return ' INDEX(`$name`)';
+    }
+
+    return null;
+  }
+
   static String compileReference(MigrationColumnReference ref) {
     var buf = StringBuffer('REFERENCES ${ref.foreignTable}(${ref.foreignKey})');
     if (ref.behavior != null) buf.write(' ${ref.behavior!}');
@@ -78,6 +86,7 @@ class MysqlTable extends Table {
 
   void compile(StringBuffer buf, int indent) {
     var i = 0;
+    List indexBuf = [];
 
     _columns.forEach((name, column) {
       var col = MySqlGenerator.compileColumn(column);
@@ -88,7 +97,16 @@ class MysqlTable extends Table {
       }
 
       buf.write('$name $col');
+
+      var index = MySqlGenerator.compileIndex(name, column);
+      if (index != null) indexBuf.add(index);
     });
+
+    if (indexBuf.isNotEmpty) {
+      for (var i = 0; i < indexBuf.length; i++) {
+        buf.write(',\n${indexBuf[$1]}');
+      }
+    }
   }
 }
 
